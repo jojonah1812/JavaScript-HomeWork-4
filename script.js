5a84b8e9908038c825994e5cdfcf668084361cc2
Jank Jank <jank.jank1812@gmail.com>
2021-05-19 23:42:17 -0700

    cannot get the timer to work

---
+++ script.js
@@ -0,0 +1,22 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+
+
+function countdown() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
