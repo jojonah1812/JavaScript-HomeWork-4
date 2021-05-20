8b92a1ffc43d944ffe5257c06eb5858f6a22ffd3
Jank Jank <jank.jank1812@gmail.com>
2021-05-20 16:55:19 -0700

    attempt at using notes from office hours, likely deleting these efforts and starting over - too complicated.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
-// countdown timer
+// countdown timer variables
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 
+//Start button
+document.getElementById("onclick").addEventListener("click", 
+function() {
+      }
+);
+
+// countdown timer 
 function countdown() {
     var timeLeft = 60;
-
     var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
         timerEl.textContent = timeLeft + ' seconds remaining';
@@ -21,3 +27,41 @@ function countdown() {
   }, 1000);
 }
 countdown ()
+
+
+//questions
+var questionCounter=0;
+
+var questionDiv = document.querySelector("#question-title")
+
+var questions = [{
+    question:”Question being asked #1?”,
+	  choices:[”Boolean, “string”, “function”, “number”]
+    correctAnswer: “Function”
+}, 
+{
+	question:”Question being asked #2?”,
+	choices:[”choice 1, “choice 2”, “choice 3”, “choice 4”]
+correctAnswer: “Function”
+	},
+  { 
+	question:” Question being asked #3?”,
+	choices:[”choice 1a, “choice 2a”, “choice 3a”, “choice 4a”]
+correctAnswer: “Function”
+}];
+
+function renderFirstQuestion(){}
+  var question1 = questions[questionCounter];
+
+  //actual question
+  var question = question1.question
+
+  var questionh2=document.createElement("h2")
+  questionh2.textContent = question;
+  questionDiv.appendChild(questionh2)
+ 
+  //array to loop through each element and make a button for each choice
+  var choices = question1.choices
+  var correctAnswer = question1.correctAnswer
+//once they answer the question, the counter increases
+  questionCounter++;
